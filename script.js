3220bcd9c3c9e7a109eee43911e42109d1b8934f
Shubham Kumar Shukla <shubhssays@gmail.com>
2024-01-21 19:30:51 +0530

    creating game board

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+//html constants
 const gameBoard = document.getElementById("game-board");
-console.log("gameBoard",gameBoard.clientHeight, gameBoard.clientWidth);
-
 const blueBoard = document.getElementById("board-blue");
-console.log("blueBoard",blueBoard.clientHeight, blueBoard.clientWidth);
+const pathHorizontal = document.getElementsByClassName("path-horizontal");
+const pathVertical = document.getElementsByClassName("path-vertical");
+
+// game constants
+const blockCount = 18;
+const pathBlockClass = "path-block"
+
+//creating horizontal path
+for(let ph of pathHorizontal){
+    for(let i = 0; i < blockCount; i++){
+        const div = document.createElement("div");
+        div.className = pathBlockClass;
+        ph.appendChild(div)
+    }
+}
+
+
+//creating vertical path
+for(let pv of pathVertical){
+    for(let i = 0; i < blockCount; i++){
+        const div = document.createElement("div");
+        div.className = pathBlockClass;
+        pv.appendChild(div)
+    }
+}
+
